3baf55382bb08b96d21f60d1665ffa68359a8a40
George Carder <georgercarder@gmail.com>
2018-09-16 16:53:52 -0700

    defined 'pseudorand'..

--- homework/exercises/exercise2_5/src/main.cpp
+++ homework/exercises/exercise2_5/src/main.cpp
@@ -2,6 +2,13 @@
 #include <iostream>
 using namespace std;
 
+int pseudorand(int p) {
+  
+  srand(p);
+
+  return rand();
+}
+
 int main(void)
 {
   /// orbit_structure_nonterminating_orbit
@@ -15,9 +22,17 @@ int main(void)
   
   
  uint64_t seed = 1234;
-  
-  srand(seed);
-  for(int i = 0; i < 500; i++)
-    cout << rand() << endl;
+ cout << seed << endl; 
+  //orbit_structure_nonterminating_orbit<>(seed, rand)
+
+ seed = pseudorand(seed);
+ cout << seed << endl;
+
+ for(int i = 0; i < 10; i++) {
+   seed = pseudorand(seed);
+   cout << seed << endl;
+ }
+
+
   return 0;
 }
